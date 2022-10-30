857269af999cc2a5b45ef5411472820197b464fc
Arnab Ghosh <arnabcare21@gmail.com>
2022-10-29 23:31:22 -0700

    Initial commits. Note that the project is broken right now

--- src/main/java/com/ansupercomputer/systemresourcemonitor/cpu/CpuController.java
+++ src/main/java/com/ansupercomputer/systemresourcemonitor/cpu/CpuController.java
@@ -1,5 +1,8 @@
 package com.ansupercomputer.systemresourcemonitor.cpu;
 
+import com.ansupercomputer.systemresourcemonitor.resource.ResourceService;
+import com.ansupercomputer.systemresourcemonitor.resource.SensorNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("cpu")
 public class CpuController {
-    @GetMapping
-    public String testEndpoint() {
-        return "Hello World!";
+
+    @Autowired
+    private ResourceService service;
+
+    public CpuController (ResourceService service) {
+        this.service = service;
+    }
+
+    @GetMapping("diagnostic")
+    public String cpuDiagnostics() throws SensorNotFoundException {
+        return service.checkSensors();
     }
 }
--- src/main/java/com/ansupercomputer/systemresourcemonitor/resource/ResourceService.java
+++ src/main/java/com/ansupercomputer/systemresourcemonitor/resource/ResourceService.java
@@ -3,23 +3,45 @@ package com.ansupercomputer.systemresourcemonitor.resource;
 import com.profesorfalken.jsensors.JSensors;
 import com.profesorfalken.jsensors.model.components.Components;
 import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.sensors.Fan;
+import com.profesorfalken.jsensors.model.sensors.Temperature;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ResourceService {
     Components components;
     public ResourceService() {
         components = JSensors.get.components();
-        try {
-            checkSensors();
-
-        } catch (Throwable e) {
-            System.out.println("Failed with exception: " + e);
-        }
     }
 
-    public void checkSensors() throws SensorNotFoundException {
+    /**
+     * Checks all the CPU sensors
+     * @return a String with a bunch of CPU diagnostics
+     * @throws SensorNotFoundException when it can't find any CPUs
+     */
+    public String checkSensors() throws SensorNotFoundException {
+        StringBuilder output = new StringBuilder();
         List<Cpu> cpus = components.cpus;
         if(cpus == null) throw new NoCpuSensorFoundException();
+        for(final Cpu cpu: cpus) {
+            output.append("CPU Name: ").append(cpu.name).append("\n");
+            if(cpu.sensors == null) throw new NoCpuSensorFoundException();
+            output.append(("Sensors: ") + "\n");
+
+            //Print temperatures
+            List<Temperature> temps = cpu.sensors.temperatures;
+            for (final Temperature temp : temps) {
+                output.append(temp.name).append(": ").append(temp.value).append(" C").append("\n");
+            }
+
+            //Print fan speed
+            List<Fan> fans = cpu.sensors.fans;
+            for (final Fan fan : fans) {
+                output.append(fan.name).append(": ").append(fan.value).append(" RPM").append("\n");
+            }
+        }
+        return output.toString();
     }
 }
