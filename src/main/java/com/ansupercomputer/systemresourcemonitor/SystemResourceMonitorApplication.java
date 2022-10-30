31abdc08bb2aae0626a16be1d71cdaac8d69e863
Arnab Ghosh <arnabcare21@gmail.com>
2022-10-29 18:06:50 -0700

    Initial commit

---
+++ src/main/java/com/ansupercomputer/systemresourcemonitor/SystemResourceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ansupercomputer.systemresourcemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemResourceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemResourceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ansupercomputer/systemresourcemonitor/SystemResourceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansupercomputer.systemresourcemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemResourceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
